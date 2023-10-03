b7a616c4d5ef6d865a42da6b7c1a981508b8f143
leolee <leoxp2008@gmail.com>
2023-10-04 01:13:46 +0800

    Update index.py
    去掉了max_results参数，只返回第一个搜索结果，并按要求返回了符合格式的JSON数据

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 
 from flask import Flask, request, jsonify
 from duckduckgo_search import DDGS
-from itertools import islice
 
 app = Flask(__name__)
 
@@ -10,24 +9,57 @@ def search():
     # 从JSON请求体中获取搜索关键词
     data = request.get_json()
     search_key = data.get('searchKey')
-    max_results = int(data.get('max_results', 10))
-    results = []
+    result = ""
 
     with DDGS() as ddgs:
         # 使用DuckDuckGo搜索关键词
         ddgs_gen = ddgs.text(search_key, safesearch='Off', timelimit='y', backend="lite")
-        # 从搜索结果中获取最大结果数
-        for r in islice(ddgs_gen, max_results):
-            results.append(r['body'])  # 提取结果中的body字段
+        # 获取第一个搜索结果
+        first_result = next(ddgs_gen, None)
+        if first_result:
+            result = first_result['body']  # 提取结果中的body字段
 
     # 返回一个json响应，包含搜索结果的body字段
     response = {
-        "prompt": results
+        "prompt": result
     }
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
+
+# from flask import Flask, request, jsonify
+# from duckduckgo_search import DDGS
+# from itertools import islice
+
+# app = Flask(__name__)
+
+# @app.route('/search', methods=['POST'])
+# def search():
+#     # 从JSON请求体中获取搜索关键词
+#     data = request.get_json()
+#     search_key = data.get('searchKey')
+#     max_results = int(data.get('max_results', 1))
+#     results = []
+
+#     with DDGS() as ddgs:
+#         # 使用DuckDuckGo搜索关键词
+#         ddgs_gen = ddgs.text(search_key, safesearch='Off', timelimit='y', backend="lite")
+#         # 从搜索结果中获取最大结果数
+#         for r in islice(ddgs_gen, max_results):
+#             results.append(r['body'])  # 提取结果中的body字段
+
+#     # 返回一个json响应，包含搜索结果的body字段
+#     response = {
+#         "prompt": results
+#     }
+#     return jsonify(response)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
 
 
 
