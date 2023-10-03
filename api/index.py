848a3e73492a7bc3bee4559e2ba07921d3303cd9
leolee <leoxp2008@gmail.com>
2023-10-04 00:33:15 +0800

    Update index.py
    增加仅输出body字段

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ def search():
     # 从JSON请求体中获取关键词和最大结果数
     data = request.get_json()
     keywords = data.get('q')
-    max_results = int(data.get('max_results', 3))
+    max_results = int(data.get('max_results', 10))
     results = []
 
     with DDGS() as ddgs:
@@ -17,13 +17,17 @@ def search():
         ddgs_gen = ddgs.text(keywords, safesearch='Off', timelimit='y', backend="lite")
         # 从搜索结果中获取最大结果数
         for r in islice(ddgs_gen, max_results):
-            results.append(r)
+            results.append(r)  # 完整的搜索结果
 
-    # 返回一个json响应，包含搜索结果
-    return jsonify({'results': results})
+    # 仅提取结果中的body字段
+    body_results = [r['body'] for r in results]
+
+    # 返回一个json响应，包含完整的搜索结果和仅包含body字段的搜索结果
+    return jsonify({'results': results, 'body_results': body_results})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
 
 
 #源代码
