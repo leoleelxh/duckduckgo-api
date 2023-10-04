36b1d3930198b0983f0e2e0af7c93ff2395200bf
leolee <leoxp2008@gmail.com>
2023-10-04 16:07:51 +0800

    Update index.py
    定义搜索结果个数

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, request, jsonify
 from duckduckgo_search import DDGS
 
@@ -5,27 +7,60 @@ app = Flask(__name__)
 
 @app.route('/search', methods=['POST'])
 def search():
-    # 从JSON请求体中获取搜索关键词
+    # 从JSON请求体中获取搜索关键词和最大结果数
     data = request.get_json()
     search_key = data.get('searchKey')
-    result = ""
+    max_results = int(data.get('max_results', 3))
+    results = []
 
     with DDGS() as ddgs:
         # 使用DuckDuckGo搜索关键词
         ddgs_gen = ddgs.text(search_key, safesearch='Off', timelimit='y', backend="lite")
-        # 获取第一个搜索结果
-        first_result = next(ddgs_gen, None)
-        if first_result:
-            result = first_result['body']  # 提取结果中的body字段
+        # 获取指定数量的搜索结果
+        for i in range(max_results):
+            result = next(ddgs_gen, None)
+            if result:
+                results.append(result['body'])  # 提取结果中的body字段
 
     # 返回一个json响应，按照指定的输出格式
     response = {
-        "prompt": f"搜索词：{search_key}, 搜索结果{result}"
+        "prompt": f"搜索词：{search_key}, 搜索结果：{results}"
     }
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
+# from flask import Flask, request, jsonify
+# from duckduckgo_search import DDGS
+
+# app = Flask(__name__)
+
+# @app.route('/search', methods=['POST'])
+# def search():
+#     # 从JSON请求体中获取搜索关键词
+#     data = request.get_json()
+#     search_key = data.get('searchKey')
+#     result = ""
+
+#     with DDGS() as ddgs:
+#         # 使用DuckDuckGo搜索关键词
+#         ddgs_gen = ddgs.text(search_key, safesearch='Off', timelimit='y', backend="lite")
+#         # 获取第一个搜索结果
+#         first_result = next(ddgs_gen, None)
+#         if first_result:
+#             result = first_result['body']  # 提取结果中的body字段
+
+#     # 返回一个json响应，按照指定的输出格式
+#     response = {
+#         "prompt": f"搜索词：{search_key}, 搜索结果{result}"
+#     }
+#     return jsonify(response)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
 
 
 
